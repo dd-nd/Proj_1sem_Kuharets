23980b615a1de57bda902b0ec96e7486db4c1c19
dd-nd <lya.yop@yandex.ru>
2022-11-25 21:34:31 +0300

    changed

--- PZ-6/6-2.py
+++ PZ-6/6-2.py
@@ -4,27 +4,27 @@ from random import randint
 try:
     n = int(input("Количество элементов в списке -> "))
     r = int(input("Введите число ->"))
+    lst = []
+    c = []
+    dop = []
+    for i in range(n): # наполняем список
+        a = randint(1, 20)
+        lst.append(a)
+    print(lst)
+    for i, x in enumerate(lst):
+        dop.append(lst[i:i+2])
+        c.append(sum(lst[i:i+2]))
+    c.pop(-1)
+    print(f'Суммы соседних чисел -> {c}')
+    def near_v(a, r): # ближайшее число к сумме соседних чисел
+        found = c[0]
+        for i in c:
+            if abs(i - r) < abs(found - r):
+                found = i
+                for j in range(len(c)):
+                    if c[j] == found:
+                        print(f'Соседние числа -> {dop[j]}')
+        return found
+    print(f'Ближайшая сумма -> {near_v(a, r)}')
 except:
     print("Error")
-lst = []
-c = []
-dop = []
-for i in range(n): # наполняем список
-    a = randint(1, 20)
-    lst.append(a)
-print(lst)
-for i, x in enumerate(lst):
-    dop.append(lst[i:i+2])
-    c.append(sum(lst[i:i+2]))
-c.pop(-1)
-print(f'Суммы соседних чисел -> {c}')
-def near_v(a, r): # ближайшее число к сумме соседних чисел
-    found = c[0]
-    for i in c:
-        if abs(i - r) < abs(found - r):
-            found = i
-            for j in range(len(c)):
-                if c[j] == found:
-                    print(f'Соседние числа -> {dop[j]}')
-    return found
-print(f'Ближайшая сумма -> {near_v(a, r)}')
