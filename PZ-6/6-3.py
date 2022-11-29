d04a7461fb1d404f0c21f9bfd46f89834a36fbda
dd-nd <lya.yop@yandex.ru>
2022-11-29 20:19:55 +0300

    changed

--- PZ-6/6-3.py
+++ PZ-6/6-3.py
@@ -2,19 +2,22 @@
 # Осуществлять сдвиг элементов списа влево на K позиций.
 # Последние K элементов полученного списка положить равными 0
 from random import randint
-
-n = int(input("Количество элементов в списке -> "))
-k = int(input("Введите число K (1 < K < N) -> "))
-# if 1 < k < len(n):
-#     k = int(input("Введите число (1 < K < N) -> "))
-lst = []
-for i in range(n):  # наполняем список
-    a = randint(1, 20)
-    lst.append(a)
-print(lst)
-x = 0
-while x < k: # сдвиг влево
-    lst.pop(0)
-    lst.append(0)
-    x += 1
-print(lst)
+try:
+    n = int(input("Количество элементов в списке -> "))
+    k = int(input("Введите число K (1 < K < N) -> "))
+    if k > n or k < 1:
+        while k > n or k < 1:
+            k = int(input("Введите число K (1 < K < N) -> "))
+    lst = []
+    for i in range(n):  # наполняем список
+        a = randint(1, 20)
+        lst.append(a)
+    print(lst)
+    x = 0
+    while x < k: # сдвиг влево
+        lst.pop(0)
+        lst.append(0)
+        x += 1
+    print(lst)
+except:
+    print("Error")
