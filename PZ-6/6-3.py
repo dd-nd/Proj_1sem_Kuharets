c6ff2bddfa13be672f6972835f00f58c156b7acc
dd-nd <lya.yop@yandex.ru>
2022-11-29 20:00:42 +0300

    changed

--- PZ-6/6-3.py
+++ PZ-6/6-3.py
@@ -4,11 +4,17 @@
 from random import randint
 
 n = int(input("Количество элементов в списке -> "))
-r = int(input("Введите число (1 < K < N) -> "))
-# if 1 < r < len(n):
-#     r = int(input("Введите число (1 < K < N) -> "))
+k = int(input("Введите число K (1 < K < N) -> "))
+# if 1 < k < len(n):
+#     k = int(input("Введите число (1 < K < N) -> "))
 lst = []
 for i in range(n):  # наполняем список
     a = randint(1, 20)
     lst.append(a)
+print(lst)
+x = 0
+while x < k:
+    lst.pop(0)
+    lst.append(0)
+    x += 1
 print(lst)
