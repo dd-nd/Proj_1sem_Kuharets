590849ca4fbb3ed02013a21d99c516a428ef53f3
dd-nd <lya.yop@yandex.ru>
2022-11-30 11:58:35 +0300

    changed

---
+++ PZ-7/7-1.py
@@ -0,0 +1,13 @@
+# Дана строка. Посчитать количество цифр в ней
+try:
+    a = input("Введите строку -> ")
+    count = 0
+    for i in a:
+        if i.isdigit():
+            count += 1
+    if count == 0:
+        print("Цифр нет")
+    else:
+        print(count)
+except:
+    print("Error")
