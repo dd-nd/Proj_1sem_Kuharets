8eedf83120b82931e688f7de24ea311a0efd07a1
dd-nd <lya.yop@yandex.ru>
2023-01-22 18:43:58 +0300

    added

--- PZ-11/11-2.py
+++ PZ-11/11-2.py
@@ -1,10 +1,28 @@
 # Из файла text18-13.txt вывести на экран его содержимое, кол-во сим-ов в тексте.
 # Сформировать новый файл, в который поместить текст в стихотворной форме,
 # предварительно вставив после строки N (N задает пользователь) произвольную фразу.
-c = 0
-f = open('text18-13.txt', 'r', encoding = 'UTF-8')
-for i in f:
-    print(i, end = '')
-    c += 1
-print(end = '\n')
-print(c)
+
+f = open('text18-13.txt', encoding='UTF-8')
+f1 = open('text18-13.txt', encoding='UTF-8')
+
+data = f.read()
+c = len(data)
+print(data)
+print(f'Кол-во символов в файле: {c}')
+try:
+    n = int(input('Введите номер строки-> '))
+    string = input('Введите строку-> ')
+except:
+    print('Error')
+lst=[]
+for line in f1.readlines():
+    line2 = line.replace('\n', '')
+    lst.append(line2)
+lst.insert(n-1, string)
+f2 = open('text18-13-new.txt','w', encoding='UTF-8')
+for item in lst:
+    f2.write('%s\n' % item)
+
+f.close()
+f1.close()
+f2.close()
