cb0e942222547c61d9f3896cd0b04eb7e4e14d3a
pochta@mail.ru <daniel.kipa@yandex.ru>
2023-01-27 15:38:46 +0300

    changed

--- PZ-12/12-2.py
+++ PZ-12/12-2.py
@@ -2,4 +2,7 @@
 # верхнему регистру.
 
 lst = ['Оттава', 'Москва', 'Пекин', 'Полоцк', 'Версаль', 'Дели', 'Каир']
-print([i.upper() for i in lst])
+
+lst_1 = [x for i in lst for x in i]
+lst_2 = [i for i in lst_1 if i in 'бвгджзклмнпрстфхцчшщБВГДЖЗКЛНМПРСТФХЦЧШЩ']
+print([i.upper() for i in lst_2])
