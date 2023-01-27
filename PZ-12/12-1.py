52f6fcb60aceac4bcd87b93a5993d0a818b82cc0
pochta@mail.ru <daniel.kipa@yandex.ru>
2023-01-27 15:13:45 +0300

    changed

--- PZ-12/12-1.py
+++ PZ-12/12-1.py
@@ -1,2 +1,13 @@
 # Проверить есть ли в последовательности целых N чисел число K
 
+from random import randint
+n = int(input("Введите кол-во чисел в списке -> "))
+k = int(input("Введите число -> "))
+f = False
+lst = [randint(0, 20) for i in range(n)]
+print(lst)
+
+for i in lst:
+    if i == k:
+        f = True
+print(f)
