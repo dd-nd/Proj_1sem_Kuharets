9707abdb66baba0b250d487854a64173784ff2df
valera <prostomister2@mail.ru>
2023-02-03 19:14:20 +0300

    changed

--- PZ-13/13-1.py
+++ PZ-13/13-1.py
@@ -1,2 +1,16 @@
 # Для каждой строки матрицы с нечетным номером найти
 # среднее арифметическое ее элементов
+from random import randint
+
+r = int(input("Введите кол-во строк -> "))
+c = int(input("Введите кол-во столбцов -> "))
+a = [[randint(0, 20) for i in range(r)] for i in range(c)]
+print(a)    # готовая матрица
+b = a[::2]
+print(b)    # нечетные строки
+x = 0
+z = 0
+for item in b:
+    x += len(item)
+    z += sum(item)
+print(f'Среднее арифметическое эл-ов -> {round((z/x), 2)}')
