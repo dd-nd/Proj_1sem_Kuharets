ddd6b32ae3cdc74d033004719287b4f2dfcf658e
valera <prostomister2@mail.ru>
2023-02-03 19:20:43 +0300

    changed

--- PZ-13/13-2.py
+++ PZ-13/13-2.py
@@ -1,2 +1,12 @@
 # В матрице найти максимальный положительный элемент
+from random import randint
 
+r = int(input("Введите кол-во строк -> "))
+c = int(input("Введите кол-во столбцов -> "))
+a = [[randint(0, 20) for i in range(r)] for i in range(c)]
+print(a)
+x = 0
+for item in a:
+    if x <= int(item):
+        x = int(item)   #er
+print(f'Максимальный эл-т -> {x}')
