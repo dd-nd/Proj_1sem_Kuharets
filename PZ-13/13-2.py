af2e25cf220beae072bbd8107c9312ac8f4094d1
dd-nd <lya.yop@yandex.ru>
2023-02-23 18:52:02 +0300

    done

--- PZ-13/13-2.py
+++ PZ-13/13-2.py
@@ -1,12 +1,16 @@
-# В матрице найти максимальный положительный элемент
+# В матрице найти максимальный положительный элемент, кратный 4
 from random import randint
 
 r = int(input("Введите кол-во строк -> "))
 c = int(input("Введите кол-во столбцов -> "))
-a = [[randint(0, 20) for i in range(r)] for i in range(c)]
+a = [[randint(-20, 23) for i in range(r)] for i in range(c)]
 print(a)
 x = 0
-for item in a:
-    if x <= int(item):
-        x = int(item)   #er
-print(f'Максимальный эл-т -> {x}')
+for i in a:
+    for item in i:
+        if x <= item and item % 4 == 0:
+            x = item
+print(x)
+# print(f'Максимальный эл-т -> {x}')
+
+# print(max(list(x for x in a)))
