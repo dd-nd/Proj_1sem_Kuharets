eb6e0da0bcb6e64b22733b98122107d4df5b1c3f
dd-nd <lya.yop@yandex.ru>
2023-02-24 16:44:45 +0300

    done

--- PZ-14/14-1.py
+++ PZ-14/14-1.py
@@ -1,25 +1,21 @@
 # В строках файла dates1.txt все даты представить 
 # в виде подстроки. Поместить в новый файл все 
 # даты февраля в формате дд/мм/гггг
-# import re
+import re
 
-# date_re = r'[0123]?\d[./\\][01]?\d[./\\]20[0123][01]'
-
-# открываем файл
-# name: str = input ("Введите имя входящего файла")
-# f = open (name, "r")  
 with open('dates1.txt', 'r') as f:
-    lines = f.readlines()
-    print(lines)
+  lines = f.readlines()
 
-# В эту переменную сохранится найденная дата
-# date_str = ''
+str_ = str(lines).replace('\\n', '').replace('\n', '').replace(',', '').replace(';', '').replace("'",'').split(' ')
 
-# for line in lines:
-  # поиск шаблона в строке
-#   re_result = re.search(date_re, line)
-  # если нашли
-#   if re_result:
-    # сохраняем в переменную и завершаем цикл поиска
-    # date_str = re_result.group(0)
-    # break
+date_re = r'[012][0-9]\.02\.2022'
+date_str = ''
+date_arr = []
+for date in str_:
+  re_result = re.search(date_re, date)
+  if re_result:
+    date_str = re_result.group(0)
+    date_arr.append(date_str.replace('.','/'))
+    date_arr.append('\n')
+    with open('f_date.txt', 'w') as f:
+      f.writelines(date_arr)
