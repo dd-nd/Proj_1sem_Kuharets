3ad5c6cfaf285b010c2862f5db0acf61eaff3377
dd-nd <lya.yop@yandex.ru>
2023-03-03 19:34:33 +0300

    changed

--- PZ-16/figures/circle/code.py
+++ PZ-16/figures/circle/code.py
@@ -3,6 +3,5 @@ default_radius = 5
 def circle_perimetr(radius):  # Вычисляет периметр
     pass
 
-
 def circle_area(radius):  # Вычисляет площадь
     pass
---
+++ PZ-16/figures/triangle/code.py
@@ -0,0 +1,8 @@
+d_a = 7
+d_b = 2
+d_c = 8
+def triangle_perimetr(a, b, c):  # Вычисляет периметр
+    pass
+
+def triangle_area(a, b, c):  # Вычисляет площадь
+    pass
