9b6db53a8e93edfaa6eeccb3f435d1d6cd3924c8
dd-nd <lya.yop@yandex.ru>
2023-03-10 14:42:41 +0300

    changed

---
+++ PZ-16/main.py
@@ -0,0 +1,11 @@
+import module
+
+from figures import circle_perimetr
+
+module.get_set()
+module.get_txt()
+print(module.file_doc.doc)
+print(module.list_doc.doc)
+print(dir(module))
+
+print(circle_perimetr(9))
